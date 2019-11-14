2d5b018751085a836517a3124e6e819e5abf9960
IrochiRoninj <boriscampos1@gmial.com>
2019-11-14 14:20:56 -0300

    primera confirmacion

--- ProyectoPetalos/core/views.py
+++ ProyectoPetalos/core/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
-
-def galeria(request):
-    flores=Flores.objects.all()
-    return render(request, 'core/galeria.html',{'listaflores':flores})    
